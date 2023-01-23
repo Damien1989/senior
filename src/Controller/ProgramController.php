8927e4b873641c94aee93b914d6f3ddfa27dc1ab
damien <contact.damiendiaz@gmail.com>
2023-01-23 14:23:03 +0100

    quest 5 routing

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/program", name="app_program")
+     */
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            'programme' => 'coder en chaussettes',
+        ]);
+    }
+
+    /**
+     * @Route("/program/{id}", methods={"GET"}, requirements={"id"="\d+"}, name="program_id")
+     */
+
+    public function show (int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+    }
+}
