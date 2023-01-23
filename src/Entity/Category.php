4f313408d275409312b1f36a48d8f44985b8049a
damien <contact.damiendiaz@gmail.com>
2023-01-23 22:01:35 +0100

    quest 11

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -16,6 +16,40 @@ class Category
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Program::class, mappedBy="category")
+     */
+
+    public function __construct()
+    {
+        $this->programs = new ArrayCollection();
+    }
+
+    public function getPrograms(): Collection
+    {
+        return $this->programs;
+    }
+
+    public function addProgram(Program $program): self
+    {
+        if (!$this->programs->contains($program)) {
+            $this->programs->add($program);
+            $program->setCategory($this);
+        }
+        return $this;
+    }
+
+    public function removeProgram(Program $program): self
+    {
+        if ($this->programs->removeElement($program)) {
+            // set the owning side to null (unless already changed)
+            if ($program->getCategory() === $this) {
+                $program->setCategory(null);
+            }
+        }
+        return $this;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
